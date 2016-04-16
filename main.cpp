46c013fee2bd48c14724cd2f653a8fa60521a147
Konrad <DarnokOk@gmail.com>
2016-04-16 13:32:06 +0200

    Functions and cases
    Function for clearing board,
    Deciding whether it's a stalemate.
    Menu after ending the game.

--- main.cpp
+++ main.cpp
@@ -52,96 +52,169 @@ void draw_board(int board_sent[][6])
     }
 }
 
-int main()
+void clear_board(int board_sent[][6])
 {
-    int board[7][6];
-
     for(int i = 0; i < 7; i++)
     {
         for(int j = 0; j < 6; j++)
         {
-            board[i][j] = -1;
+            board_sent[i][j] = -1;
         }
     }
+}
+
+int main()
+{
+    int board[7][6];
 
     bool play = true;
     bool first_turn = true;
+    bool game_started = true;
+    bool game_ended;
+
     short int player = 0;
 
     do
     {
         if(first_turn)
         {
-            cout << "Welcome to Connect Four game!" << endl;
+            if(game_started)
+            {
+                cout << endl << "Welcome to Connect Four game!" << endl;
+
+                //choose 2 players or AI
+
+                game_started = false;
+            }
+
+            clear_board(board);
+
+            game_ended = false;
+
             first_turn = false;
         }
 
-        cout << endl;
+        bool stalemate = true;
 
-        draw_board(board);
-
-        player = (player + 1) % 2;
-
-        cout << endl << "Your turn \"";
-
-        if(player)
+        for(int i = 0; i < 7; i++)
         {
-            cout << "O";
-        }
-        else
-        {
-            cout << "X";
-        }
-
-        cout << "\". Choose a column: ";
-
-        bool wrong_number = true;
-        int choice;
-        bool another_try = false;
-
-        do
-        {
-            if(another_try)
+            if(board[i][0] == -1)
             {
-                cout << "Invalid input! Choose a column: ";
-            }
-            cin >> choice;
-
-            //check whether it is an int at first
-
-            if( (choice > 7 && choice < 1) || (board[choice - 1][0] != -1) )
-            {
-                another_try = true;
-            }
-            else
-            {
-                wrong_number = false;
-            }
-
-        }while(wrong_number);
-
-        for(int i = 5; i >= 0; i--)
-        {
-            if(board[choice - 1][i] == -1)
-            {
-                board[choice - 1][i] = player;
+                stalemate = false;
                 break;
             }
         }
-        //here we have choice and we should add it to the board
 
-        //then check whether someone won, it's still in progress or that it's a stalemate
+        //check whether someone won
 
-        /*
-        if(true)//game done and they dont want to play anymore
+        if(stalemate) game_ended = true;// or if won
+
+        if(game_ended)
         {
-            play = false;
+            cout << endl << "Here's the final look of the board:" << endl << endl;
+
+            draw_board(board);
+
+            cout << endl;
+
+            if(stalemate)
+            {
+                cout << endl << "Noone won!" << endl;
+            }
+            //else if won
+
+            cout << endl << "Decide:"
+                 << endl << "1 - rematch"
+                 << endl << "2 - back to menu"
+                 << endl << "3 - exit"
+                 << endl << "Your choice: ";
+            short int what_now;
+            cin >> what_now;
+
+            //also check if it's an int
+
+            if(what_now > 3 || what_now < 1)
+            {
+                while(what_now > 3 || what_now < 1)
+                {
+                    cout << "Invalid input! Choose again:";
+                    cin >> what_now;
+                }
+            }
+
+            switch(what_now)
+            {
+                case 2:
+                    game_started = true;
+                case 1:
+                    first_turn = true;
+                    break;
+                case 3:
+                    play = false;
+                    break;
+            }
+
         }
-        */
+        else
+        {
+            cout << endl;
+
+            draw_board(board);
+
+            player = (player + 1) % 2;
+
+            cout << endl << "Your turn \"";
+
+            if(player)
+            {
+                cout << "O";
+            }
+            else
+            {
+                cout << "X";
+            }
+
+            cout << "\". Choose a column: ";
+
+            bool wrong_number = true;
+            short int choice;
+            bool another_try = false;
+
+            do
+            {
+                if(another_try)
+                {
+                    cout << "Invalid input! Choose a correct column: ";
+                }
+                cin >> choice;
+
+                //check whether it is an int at first
+
+                if( (choice > 7 && choice < 1) || (board[choice - 1][0] != -1) )
+                {
+                    another_try = true;
+                }
+                else
+                {
+                    wrong_number = false;
+                }
+
+            }while(wrong_number);
+
+            for(int i = 5; i >= 0; i--)
+            {
+                if(board[choice - 1][i] == -1)
+                {
+                    board[choice - 1][i] = player;
+                    break;
+                }
+            }
+        }
 
     } while(play);
 
     cout << endl;
+
     system("pause");
 
     return 0;
